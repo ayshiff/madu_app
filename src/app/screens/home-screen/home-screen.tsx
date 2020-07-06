5d1e04c8298530f827e0d76eedabd3f4ff7b67da
Panathes <dianketohead@gmail.com>
2020-07-06 11:09:49 +0200

    add styled component in leaderboard screen and map screen

--- src/app/screens/home-screen/home-screen.tsx
+++ src/app/screens/home-screen/home-screen.tsx
@@ -1,29 +1,25 @@
 import * as React from 'react';
-import {
-    View,
-    Text,
-    Image
-} from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { Screen } from '../../components';
 import { color, spacing } from '../../theme';
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 const Full = styled.View`
-    flex: 1; 
-    background-color: #FFFFFF;
-`
+    flex: 1;
+    background-color: #ffffff;
+`;
 
 const Bold = styled.Text`
     font-weight: bold;
-`
+`;
 
 const HeaderContainer = styled.View`
     height: 225;
-    background-color: #FEC530;
+    background-color: #fec530;
     border-bottom-left-radius: 30;
     border-bottom-right-radius: 30;
-`
+`;
 
 const Header = styled.View`
     flex: 1;
@@ -32,14 +28,13 @@ const Header = styled.View`
     height: 64;
     margin-left: 15;
     margin-right: 15;
-    margin-top: 34    
-`
+    margin-top: 34;
+`;
 
 const ChallengeContainer = styled.View`
     margin-top: -80;
     align-items: center;
-`
-
+`;
 
 const ChallengeView = styled.View`
     height: 115;
@@ -56,41 +51,40 @@ const ChallengeView = styled.View`
     shadow-opacity: 0.25;
     shadow-radius: 3.84;
     elevation: 5;
-`
+`;
 
 const ChallengeInfo = styled.View`
     justify-content: space-around;
     margin-left: 15;
     padding-top: 10;
     padding-bottom: 10;
-`
-
+`;
 
 const BusinessName = styled.Text`
-    color: #8E8E93;
+    color: #8e8e93;
     margin-bottom: 5;
-`
+`;
 
 const ChallengePic = styled.Image`
     margin-top: 20;
     height: 75;
     width: 75;
-`
+`;
 
 const ChallengeTitle = styled.Text`
     margin-bottom: 5;
     font-size: 20;
     justify-content: flex-start;
-`
+`;
 
 const TextContainer = styled.View`
     margin-top: 16;
-`
+`;
 
 const ProfilePic = styled.Image`
     width: 64;
     height: 64;
-`
+`;
 
 const PointsTag = styled.View`
     border-radius: 28;
@@ -100,27 +94,26 @@ const PointsTag = styled.View`
     justify-content: center;
     padding-left: 14;
     padding-right: 14;
-    background-color: #E3FFE8;
-`
+    background-color: #e3ffe8;
+`;
 
 const PointsText = styled.Text`
-    color: #70B32D;
-`
-
+    color: #70b32d;
+`;
 
 const TypeTag = styled.View`
     border-radius: 20;
-    border-color: #FE6D1A;
+    border-color: #fe6d1a;
     border-width: 1;
     align-items: center;
     justify-content: center;
     width: 100;
-`
+`;
 
 const TypeText = styled.Text`
     justify-content: center;
-    color: #FE6D1A
-`
+    color: #fe6d1a;
+`;
 
 export interface HomeScreenProps {
     navigation: any;
@@ -155,7 +148,7 @@ const Home = (/* props: HomeScreenProps */) => {
                                 </TypeTag>
                                 <PointsTag>
                                     <PointsText>80🌱</PointsText>
-                                </PointsTag >
+                                </PointsTag>
                             </ChallengeInfo>
                         </ChallengeView>
                     </View>
--- src/app/screens/leaderboard-screen/leaderboard-screen.tsx
+++ src/app/screens/leaderboard-screen/leaderboard-screen.tsx
@@ -1,103 +1,93 @@
 import * as React from 'react';
-import {
-    View,
-    Text,
-} from 'react-native';
+import { View, Text } from 'react-native';
 import { color } from '../../theme/color';
 import styled from 'styled-components/native';
 
-
 const Full = styled.View`
-    flex: 1; 
-    background-color: #FFFFFF;
-`
+    flex: 1;
+    background-color: #ffffff;
+`;
 
 const HeaderContainer = styled.View`
     height: 225;
     background-color: rgba(138, 180, 255, 0.6);
     border-bottom-left-radius: 30;
-    border-bottom-right-radius: 30 
-`
+    border-bottom-right-radius: 30;
+`;
 
 const HeaderTextContainer = styled.View`
     margin-top: 66;
     margin-left: 21;
-`
+`;
 
 const HeaderTitle = styled.Text`
     font-size: 24;
     font-weight: bold;
     color: #153733;
-`
+`;
 
 const HeaderSubtitle = styled.Text`
     font-size: 14;
     color: rgba(21, 55, 51, 0.6);
-`
+`;
 
 const HeaderTimer = styled.Text`
     font-size: 36;
-    color: #FFFFFF
-`
+    color: #ffffff;
+`;
 
 const LeaderboardContainer = styled.View`
     margin-top: 32;
     width: 100%;
     flex-direction: row;
     justify-content: space-around;
-`
-
-
+`;
 
 const LeaderboardProfile = styled.View`
-    flex-direction: row
-`
-
+    flex-direction: row;
+`;
 
 const LeaderBoardRankContainer = styled.View`
     margin-top: 7;
     margin-right: 11;
-`
+`;
 
-    
 const LeaderboardPointsContainer = styled.View`
     flex-direction: row;
     margin-top: 10;
-`
-
+`;
 
 const LeaderboardInformation = styled.View`
     flex-direction: row;
-`
-
+`;
 
 const LeaderboardUp = styled.Image`
     width: 11;
     height: 11;
-`
+`;
 
 const LeaderboardProfileName = styled.Text`
     margin-top: 4;
     font-size: 14;
     font-weight: bold;
-`
+`;
 
 const LeaderboardProfilDepartment = styled.Text`
     font-size: 12;
-`
+`;
 
 const LeaderboardProfilePic = styled.Image`
     width: 48;
     height: 48;
     margin-right: 11;
-`
+`;
 
 const LeaderboardCrown = styled.Image`
     width: 14;
     height: 10;
     margin-top: 4;
     margin-right: 5;
-`
+`;
 
 export interface LeaderboardScreenProps {
     loadContent: () => string;
@@ -111,9 +101,7 @@ export const LeaderboardScreen = ({ navigation }: LeaderboardScreenProps) => {
             <HeaderContainer>
                 <HeaderTextContainer>
                     <HeaderTitle>Classement Entreprise</HeaderTitle>
-                    <HeaderSubtitle>
-                        Renouvelé chaque semaine
-                    </HeaderSubtitle>
+                    <HeaderSubtitle>Renouvelé chaque semaine</HeaderSubtitle>
                     <HeaderTimer>5j 20h 49mn</HeaderTimer>
                 </HeaderTextContainer>
             </HeaderContainer>
