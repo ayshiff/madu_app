b47fb6b843a280240d2e8053e68a14ed71ec64b4
Panathes <dianketohead@gmail.com>
2020-07-09 09:59:34 +0200

    screen change in home-navigator

--- src/app/navigation/home-navigator.tsx
+++ src/app/navigation/home-navigator.tsx
@@ -22,7 +22,6 @@ import { MapParamList } from './types';
 import { DetailScreen } from '../screens/detail-screen/detail-screen';
 import { PoiScreen } from '../screens/poi-screen/poi-screen';
 import { PoiSuccessScreen } from '../screens/poi-success-screen/poi-success-screen';
-import { ChallengePictureStep } from '../components/templates/challenge/challenge-picture-step';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator<MapParamList>();
@@ -88,7 +87,7 @@ export const HomeNavigator = React.forwardRef<
         >
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Map" component={MapNavigator} />
-            <Tab.Screen name="Leaderboard" component={ChallengePictureStep} />
+            <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
             <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
     );
